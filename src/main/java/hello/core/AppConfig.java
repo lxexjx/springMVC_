420c34fb9f40a1512b428cf20610491bde3f9fdd
lxexjx <eun.leee1377@gmail.com>
2022-01-30 17:41:48 +0900

    여기까지 spring으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,31 +2,39 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPoicy;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration  //application의 설정정보를 담당
 //application전체를 설정하고 구성하는 자바, 구현 객체를 생성!!
 public class AppConfig {
     
     //생성자 주입!!!
     //리팩터링: 역할이 드러남. 역할의 구현이 나타남.
+
+    @Bean   //각 메서드에 bean이라 작성->spring container에 등록
     public MemberService memberService(){       //MemberService 구현객체가 생성되고
         return  new MemberServiceImpl(memberRepository());
     }
-
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public  OrderService orderService(){
         return  new OrderServiceImpl(memberRepository(),discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return  new FixDiscountPolicy();
+        //return  new FixDiscountPolicy();
+        return new RateDiscountPoicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {    //psvm
 
-        AppConfig appConfig=new AppConfig();
-        MemberService memberService =appConfig.memberService();
+//        AppConfig appConfig=new AppConfig();
+//        MemberService memberService =appConfig.memberService();->직접 찾았다면
+
+        //springcontainer:config에있는 설정정보로 ApplicationContext객체를 관리, 컨테이너로 찾아와
+        ApplicationContext applicationContext= new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class); // 이 객체를 찾을 거야!(이름,타입)
+
         //MemberService memberService=new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);  //ctrl+alt+v: 자동완성
         memberService.join(member); //member넣고 회원가입시킴
