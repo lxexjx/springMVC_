e30e05968763721c3ecaf11025e51d3a9cd99a1d
lxexjx <eun.leee1377@gmail.com>
2022-01-30 15:05:04 +0900

    AppConfig

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -10,11 +11,22 @@ import hello.core.order.OrderServiceImpl;
 
 //application전체를 설정하고 구성하는 자바, 구현 객체를 생성!!
 public class AppConfig {
+    
+    //생성자 주입!!!
+    //리팩터링: 역할이 드러남. 역할의 구현이 나타남.
     public MemberService memberService(){       //MemberService 구현객체가 생성되고
-        return  new MemberServiceImpl(new MemoryMemberRepository());
-        //생성자 주입!!!
+        return  new MemberServiceImpl(memberRepository());
     }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public  OrderService orderService(){
-        return  new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return  new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return  new FixDiscountPolicy();
     }
 }
