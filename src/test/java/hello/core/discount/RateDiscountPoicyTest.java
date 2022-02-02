fa9f48c717b88a1f137eabe6b22b43ed5c3f4103
lxexjx <eun.leee1377@gmail.com>
2022-02-02 16:43:29 +0900

    새로운 할인 정책 개발(RateDiscountPolicy)

--- src/test/java/hello/core/discount/RateDiscountPoicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPoicyTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+//Assertions.assertThat(discount).isEqualTo();를 static으로
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,21 +16,19 @@ class RateDiscountPoicyTest {
 
     @Test
     @DisplayName("VIP는10%할인이 적용됨")  //한글로 쓸 수 있음
-
     void vip_o(){
-        //given
+        //given 멤버 만들고
         Member member= new Member(1L,"memberVIP", Grade.VIP);
         //when
         int discount=discountPoicy.discount(member,10000);
-        //then
+        //then 결과
         assertThat(discount).isEqualTo(1000);    //alt+emter로 static method로 만들어주는게 좋아=>Assertions.assertThat(discount).isEqualTo(1000);가
     }
 
     @Test
-    @DisplayName("VIP가 아니면 할이니 적용 안됨")
-
+    @DisplayName("VIP가 아니면 할인이 적용 안됨")
     void vip_x(){
-        //given
+        //given 멤버만들고
         Member member= new Member(2L,"memberBASIC", Grade.BASIC);
         //when
         int discount=discountPoicy.discount(member,10000);
