ddc49a58a0b5d23a5bd6af1cb4245a7a53be380b
lxexjx <eun.leee1377@gmail.com>
2022-02-05 16:11:31 +0900

    spring으로 전환

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -13,9 +13,11 @@ public class MemberApp {
 //        AppConfig appConfig=new AppConfig();
 //        MemberService memberService =appConfig.memberService();->직접 찾았다면. memberService여기에 memberServiceImpl있어서 appconfig에서 memberServiceImpl가 객체를 생성해서  주입해
 
-        //springcontainer:config에있는 설정정보로 ApplicationContext객체를 관리, 컨테이너로 찾아와
+        /*스프링으로 전환하기!!! 스프링은 ApplicationContext로 시작, 스프링 컨테이너
+        springcontainer:config에있는 설정정보로 ApplicationContext객체(@Bean)를 관리, 컨테이너로 찾아와
+        =>AppConfig에 있는 환경설정 정보를 가지고  스프링이 안에 있는 @Bean을 스프링컨테이너에 객체 생성한거를 다 넣어놓고 관리애        * */
         ApplicationContext applicationContext= new AnnotationConfigApplicationContext(AppConfig.class);
-        MemberService memberService = applicationContext.getBean("memberService",MemberService.class); // 이 객체를 찾을 거야!(이름,타입)
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class); // memberService이 객체를 찾을 거야!(이름,타입:MemberService.class)근데 스프링 컨테이너를 사용해서 찾아와
 
         //회원가입
         //MemberService memberService=new MemberServiceImpl(); 기존에 직접 생성했다면
