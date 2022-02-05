adf693fcadd34a0bfb5e354eaadb59c6b333836a
lxexjx <eun.leee1377@gmail.com>
2022-02-05 17:01:22 +0900

    스프링 빈 조회- 동일한 타입이 둘이상

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -1,4 +1,34 @@
 package hello.core.beanfind;
 
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 public class ApplicationContextSameBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig); //이 SameBeanConfig 컨피그만 가지고 실행
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이사 있으면 중복 오류 발생")
+    void findBeanByTtpeDupliate(){
+       MemberRepository bean= ac.getBean(MemberRepository.class);
+    }
+
+    @Configuration
+    static class SameBeanConfig{
+
+        @Bean
+        public MemberRepository memberRepository1(){
+            return  new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2(){
+            return  new MemoryMemberRepository();
+        }
+    }
 }
