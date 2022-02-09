6083adab40551cd15a28b8273f982bcad3d2898e
lxexjx <eun.leee1377@gmail.com>
2022-02-10 00:36:15 +0900

    add commit싱글톤 패턴

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -28,4 +28,20 @@ public class SingletonTest {
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
 
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1=SingletonService.getInstance();
+        SingletonService singletonService2=SingletonService.getInstance();
+
+        System.out.println("singletonService1="+singletonService1);
+        System.out.println("singletonService2="+singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
+        //samw==
+        //equls
+    }
+
 }
