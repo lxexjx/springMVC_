83f816430ed36de8623c64aa71a93d344d03fdc7
lxexjx <eun.leee1377@gmail.com>
2022-02-10 00:48:29 +0900

    add commit싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -7,6 +7,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
     @Test
@@ -42,6 +44,25 @@ public class SingletonTest {
 
         //samw==
         //equls
+    }
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+
+    void springContainer(){
+        //AppConfig appConfig=new AppConfig();
+        ApplicationContext ac= new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1= ac.getBean("memberService",MemberService.class);
+        MemberService memberService2= ac.getBean("memberService",MemberService.class);
+
+        //참조값 다른 지 확인
+        System.out.println("memberService1="+memberService1);
+        System.out.println("memberService2="+memberService2);
+
+        //memberService1=! memberSErvice2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
+
     }
 
 }
