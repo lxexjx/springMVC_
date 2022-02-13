9506ba5af2156efad5bae205ba9c4e8d55f452b2
lxexjx <eun.leee1377@gmail.com>
2022-02-13 14:17:57 +0900

    add commit 탐색위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member" ,    //여기부터 하위패키지를 찾아가, memeber만 component대상
+        //basePackageClasses = ,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
 )
 public class AutoAppConfig {
-//너 오늘 진짜 죽었다 이따가 코박게 해줘
+
 }
