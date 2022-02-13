eaff480ba198c6c5b19a06906583b0c54475f7c9
lxexjx <eun.leee1377@gmail.com>
2022-02-13 17:58:57 +0900

    commit Lombok 설정

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//자동으로 getter,setter,생성자 만들어줘
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+
+        HelloLombok helloLombok=new HelloLombok();
+        helloLombok.setName("asdf");
+
+        String name=helloLombok.getName();
+        System.out.println("name="+name);
+
+        System.out.println("helloLombok="+helloLombok);
+    }
+}
