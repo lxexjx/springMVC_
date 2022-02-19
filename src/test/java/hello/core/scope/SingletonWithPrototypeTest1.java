d5940caf4d543d2b3e8650987a0219f8d5918ea6
lxexjx <eun.leee1377@gmail.com>
2022-02-19 16:03:03 +0900

    commit prototype with singletonBean 사용시 문제점

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,8 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -22,6 +24,39 @@ public class SingletonWithPrototypeTest1 {
         PrototypeBean prototypeBean2=ac.getBean(PrototypeBean.class);
         prototypeBean1.addCount();
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUserPrototype(){
+        AnnotationConfigApplicationContext ac=new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+        ClientBean clientBean1=ac.getBean(ClientBean.class);
+        int count1= clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2=ac.getBean(ClientBean.class);
+        int count2= clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+
+    }
+    @Scope("singleton")
+    static class ClientBean{
+        //private final PrototypeBean prototypeBean;  //생성전에 주입
+
+        @Autowired
+        ApplicationContext applicationContext;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean){
+//            this.prototypeBean=prototypeBean;
+//        }
+
+        public int logic(){
+             PrototypeBean prototypeBean=applicationContext.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            int count=prototypeBean.getCount();
+            return count;
+
+        }
     }
     @Scope("prototype")
     static class PrototypeBean{
